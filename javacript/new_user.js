7009e312f977d189c0572550ca3803e87a555ac1
R4JV4N5 <rajvenniyar65@gmail.com>
2022-09-03 10:18:58 +0530

    changes
    made changes to u login and linked membership and ulogin site.
    new user form created .working on validation

---
+++ javacript/new_user.js
@@ -0,0 +1,28 @@
+
+function validate(){
+
+    var firstname=document.getElementById("fname")
+    var lastname=document.getElementById("lname")
+    var phnum=document.getElementById("phno")
+    var age=document.getElementById("age")
+    var password_1=document.getElementById("pwd_1")
+    var password_2=document.getElementById("pwd_2")
+
+
+    if(firstname.value.trim()=="" || lastname.value.trim()==""){
+        alert("fields blank");
+        return false
+    }
+    else if(phnum.value.length>10 || phnum.value.length<10){
+        alert("num bank");
+        phnum.style.border="solid 3px red";
+        return false;
+
+    }
+    else{
+        alert(phnum);
+        return true;
+    }
+
+
+}
