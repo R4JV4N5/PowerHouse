6605e05d0993887df1983c3f2ec0fd3c4007fe92
Parth25402 <parthrambhia25@gmail.com>
2022-10-25 23:27:00 +0530

    js

--- javacript/new_user.js
+++ javacript/new_user.js
@@ -1,27 +1,22 @@
+        function show_notification(){
+            const notification= new Notification(' hey ',{
+                body:"<count> members are present in the gym"
 
-function validate(){
-
-    var firstname=document.getElementById("fname")
-    var lastname=document.getElementById("lname")
-    var phnum=document.getElementById("phno")
-    var age=document.getElementById("age")
-    var password_1=document.getElementById("pwd_1")
-    var password_2=document.getElementById("pwd_2")
-
-    if(firstname.value.trim()=="" || lastname.value.trim()==""){
-        alert("fields blank");
-        return false
-    }
-    else if(phnum.value.length>10 || phnum.value.length<10){
-        alert("num bank");
-        phnum.style.border="solid 3px red";
-        return false
-
-    }
-    else{
-        alert(phnum);
-        return true
-    }
+            });
+        }
+         Notification.requestPermission();
+        // access permissions and checking
+        if  (Notification.permission==="granted"){
+            show_notification();
+            setInterval(show_notification(),7)
+        }
+        else if(Notification.permission!=="denied"){
+            Notification.requestPermission().then(permission=>{
+                if(permission==="granted"){
+                    show_notification();
+                    setInterval(show_notification(),7)
+                }
+            });
 
 
-}
+        }
